120ce7fbe242a80ed589167e2c573af810e6dfab
GiovanniNoRocket <gpalenciosorio@hotmail.com>
2022-12-01 21:52:59 -0500

    Proyecto con correciones f_chat_template

--- lib/ui/controllers/chat_controller.dart
+++ lib/ui/controllers/chat_controller.dart
@@ -33,19 +33,15 @@ class ChatController extends GetxController {
 
     // TODO
     // newEntryStreamSubscription = databaseReference - child msg - child chatKey - listen
-    newEntryStreamSubscription = databaseReference.child("msg").child(chatKey).onChildAdded.listen(_onEntryAdded);
 
     // TODO
     //  updateEntryStreamSubscription = databaseReference - child msg - child chatKey - listen
-    updateEntryStreamSubscription = databaseReference.child("msg").child(chatKey).onChildChanged.listen(_onEntryChanged);
   }
 
   // método en el que cerramos los streams
   void unsubscribe() {
     //TODO
     // cancelar las subscripciones a los streams
-    newEntryStreamSubscription.cancel();
-    updateEntryStreamSubscription.cancel();
   }
 
   // este método es llamado cuando se tiene una nueva entrada
@@ -97,11 +93,6 @@ class ChatController extends GetxController {
     try {
       // TODO
       // databaseReference - child('msg') - child(key) - push() - set({'senderUid': senderUid, 'msg': msg})
-      databaseReference
-        .child('msg')
-        .child(key)
-        .push()
-        .set({'senderUid': senderUid, 'msg': msg});
     } catch (error) {
       logError(error);
       return Future.error(error);
